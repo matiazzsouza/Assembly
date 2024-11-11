cce547c3fe961a71d1605c34d7669783db004724
matiazzsouza <mate4338@gmail.com>
2024-11-10 22:58:23 -0300

    logica das embarcações

--- PROJETO_FINAL/mapa.asm
+++ PROJETO_FINAL/mapa.asm
@@ -2,19 +2,35 @@
 .stack 100h
 
 .data
-mapa db '    0 1 2 3 4 5 6 7 8 9', 13, 10
-     db '    _ _ _ _ _ _ _ _ _ _', 13, 10
-     db 'A  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'B  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'C  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'D  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'E  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'F  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'G  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'H  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'I  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db 'J  |_|_|_|_|_|_|_|_|_|_|', 13, 10
-     db '$'  ; $ indica o final da string para exibição com a função 09h do DOS
+        ; Mensagem de tentativas restantes
+        tentativas db 'Tentativas restantes -> $',10,13
+
+        ; Mapa do jogo
+        mapa db '    0 1 2 3 4 5 6 7 8 9', 13, 10
+             db '    _ _ _ _ _ _ _ _ _ _', 13, 10
+             db 'A  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'B  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'C  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'D  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'E  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'F  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'G  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'H  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'I  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db 'J  |_|_|_|_|_|_|_|_|_|_|', 13, 10
+             db '$'
+
+        ; Definição dos vetores de embarcações (cada par representa linha e coluna)
+        encouracado db 2, 3, 2, 4, 2, 5, 2, 6   ; 4 posições para o encouraçado
+        fragata db 5, 7, 5, 8                   ; 2 posições para a fragata
+        submarino1 db 10, 3                      ; 1 posição para o submarino 1
+        submarino2 db 3, 5                      ; 1 posição para o submarino 2
+        hidroaviao1 db 6, 2, 7, 2               ; 2 posições para o hidroavião 1
+        hidroaviao2 db 4, 9, 5, 9               ; 2 posições para o hidroavião 2
+
+        ; Mensagens de feedback
+        msg_acerto db 'Acertou uma parte da embarcacao! $', 10, 13
+        msg_agua db 'Acertou a agua! $', 10, 13
 
 .code
 main:
@@ -29,16 +45,12 @@ loop_inicio:
     ; Ler coordenadas do usuário
     call ler_coordenadas
 
-    ; Verificar se o usuário deseja sair (pressionando 'q' ou 'Q')
-    cmp al, 'q'
-    je fim_programa
-    cmp al, 'Q'
-    je fim_programa
-
+    ; Verificar se acertou alguma embarcação
+    call verificar_acerto
+    
     ; Atualizar o mapa com a coordenada fornecida
     call atualizar_mapa
 
-    ; Voltar para o início do loop
     jmp loop_inicio
 
 fim_programa:
@@ -58,6 +70,12 @@ ler_coordenadas:
     ; Ler a letra (A-J) da linha
     mov ah, 01h
     int 21h
+
+    cmp al, 'q'
+    je fim
+    cmp al, 'Q'
+    je fim
+
     sub al, 'A'           ; Converte para índice (0-9)
 
     ; Armazenar no registrador BL a linha (0-9)
@@ -70,8 +88,58 @@ ler_coordenadas:
 
     ; Armazenar no registrador BH a coluna (0-9)
     mov bh, al
+
+    fim:
     ret
 
+; Função para verificar se acertou alguma embarcação
+verificar_acerto:
+    ; Usa registradores BL e BH para armazenar linha e coluna fornecidas
+    mov al, bl       ; Linha em al (0-9)
+    mov ah, bh       ; Coluna em ah (0-9)
+
+    ; Checar no encouraçado
+    lea si, encouracado
+    mov cx, 4        ; 4 partes do encouraçado
+loop_encouraçado:
+    cmp al, [si]     ; Compara linha
+    jne prox_verificacao_enc
+    cmp ah, [si + 1]   ; Compara coluna
+    jne prox_verificacao_enc
+
+    ; Se acertar, marca o ponto como atingido e exibe "Acertou uma parte!"
+    call mensagem_acerto
+    mov byte ptr [si], -1   ; Marca posição atingida
+    mov byte ptr [si + 1], -1 ; Marca posição atingida
+    jmp fim_verificar
+
+prox_verificacao_enc:
+    add si, 2        ; Move para a próxima coordenada
+    loop loop_encouraçado
+
+    ; Repetir lógica acima para fragata, submarinos, hidroaviões
+
+    ; Caso não acerte nenhuma posição
+    call mensagem_agua
+
+fim_verificar:
+    ret
+
+; Mensagem para acerto de parte da embarcação
+mensagem_acerto:
+    mov ah, 09h
+    lea dx, msg_acerto
+    int 21h
+    ret
+
+; Mensagem para acerto da água (erro)
+mensagem_agua:
+    mov ah, 09h
+    lea dx, msg_agua
+    int 21h
+    ret
+
+; Função para atualizar o mapa com um 'X' quando o usuário acerta uma posição
 atualizar_mapa:
     ; Calcular o deslocamento da linha: cada linha ocupa 24 caracteres no total
     mov al, bl            ; Linha (0-9) está em BL
